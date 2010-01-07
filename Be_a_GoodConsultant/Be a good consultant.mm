3198218d02ad3d332b6735c2d0ae1aa94d880aa8
Alec Clews <alec.clews@voga.com.au>
2010-01-07 19:03:07 +1100

    Be a Good Consultant

---
+++ Be_a_GoodConsultant/Be a good consultant.mm
@@ -0,0 +1,198 @@
+<map version="0.9.0_Beta_8">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1139394674772" ID="Freemind_Link_91793656" MODIFIED="1139394691626" TEXT="Be a good&#xa;consultant">
+<node CREATED="1139446433628" ID="Freemind_Link_335337623" MODIFIED="1150237494984" POSITION="right" TEXT="Know your vision">
+<node CREATED="1139394743621" ID="Freemind_Link_1458630786" MODIFIED="1139394757851" TEXT="Your value proposition">
+<node CREATED="1150238674875" ID="Freemind_Link_1065520155" MODIFIED="1150238686375" TEXT="The value that the customer receives"/>
+<node CREATED="1141216017656" ID="Freemind_Link_1875087936" MODIFIED="1141216031486" TEXT="Gets you the business"/>
+<node CREATED="1141193033797" ID="Freemind_Link_1717103045" MODIFIED="1141193877167" TEXT="Never based around the&#xa;idea that you are cheap">
+<arrowlink DESTINATION="Freemind_Link_1131964730" ENDARROW="Default" ENDINCLINATION="290;0;" ID="Freemind_Arrow_Link_1060415409" STARTARROW="None" STARTINCLINATION="290;0;"/>
+</node>
+<node CREATED="1139394766724" ID="Freemind_Link_491923946" MODIFIED="1150238238406" TEXT="What make you&#xa;different to the other guy/gal">
+<node CREATED="1141193094642" ID="Freemind_Link_1522299538" MODIFIED="1150238264375" TEXT="Unique skills, experience&#xa;and processes">
+<arrowlink DESTINATION="Freemind_Link_1717103045" ENDARROW="Default" ENDINCLINATION="171;0;" ID="Freemind_Arrow_Link_1511224407" STARTARROW="None" STARTINCLINATION="171;0;"/>
+</node>
+<node CREATED="1146178431488" ID="Freemind_Link_1266728457" MODIFIED="1146178472032" TEXT="Your vision for your customer.&#xa;Where can you take them?"/>
+</node>
+<node CREATED="1139394783658" ID="Freemind_Link_1987585079" MODIFIED="1150238708140" TEXT="Should be specific to the engagement">
+<node CREATED="1141193338211" ID="Freemind_Link_1591952217" MODIFIED="1141193357889" TEXT="Need to understand&#xa;customers needs">
+<node CREATED="1141193359341" ID="Freemind_Link_1131964730" MODIFIED="1141193877167" TEXT="Takes time&#xa;and effort"/>
+</node>
+</node>
+<node CREATED="1141216067488" ID="Freemind_Link_225356734" MODIFIED="1146178566322" TEXT="Must be part of you personal values">
+<arrowlink DESTINATION="Freemind_Link_325567240" ENDARROW="Default" ENDINCLINATION="418;0;" ID="Freemind_Arrow_Link_2302276" STARTARROW="None" STARTINCLINATION="418;0;"/>
+</node>
+<node CREATED="1139395541774" ID="Freemind_Link_1297534364" MODIFIED="1139738001585" TEXT="Know your limitations">
+<arrowlink DESTINATION="Freemind_Link_325567240" ENDARROW="Default" ENDINCLINATION="0;-10;" ID="Freemind_Arrow_Link_576565373" STARTARROW="None" STARTINCLINATION="-146;4;"/>
+</node>
+</node>
+<node CREATED="1139395027374" ID="Freemind_Link_1022146933" MODIFIED="1150238297437" TEXT="Your objectives">
+<node CREATED="1139395037689" ID="Freemind_Link_911705316" MODIFIED="1141182520734" TEXT="Get payed"/>
+<node CREATED="1139395047593" ID="Freemind_Link_805159683" MODIFIED="1139395051609" TEXT="Repeat business">
+<node CREATED="1150238390796" ID="Freemind_Link_1617341455" MODIFIED="1150238428000" TEXT="Beware cutting prices on the&#xa;&apos;promise&apos; of repeat business">
+<node CREATED="1150238451234" ID="Freemind_Link_912890856" MODIFIED="1150238483296" TEXT="You must cover ALL costs somehow"/>
+<node CREATED="1150238484609" ID="Freemind_Link_1605584910" MODIFIED="1150238496515" TEXT="Can b very hard to raise prices later"/>
+<node CREATED="1150238499781" ID="Freemind_Link_675980294" MODIFIED="1150238516421" TEXT="Promises are often not fullfilled"/>
+<node CREATED="1150238566078" ID="Freemind_Link_1446930936" MODIFIED="1150238576484" TEXT="Devalues what you are offering"/>
+</node>
+<node CREATED="1150259279984" ID="Freemind_Link_777232162" MODIFIED="1150259313781" TEXT="But have a discount for&#xa;longer commitments"/>
+</node>
+<node CREATED="1139395052570" ID="Freemind_Link_1669709887" MODIFIED="1139395058669" TEXT="Recommendation"/>
+<node CREATED="1139395383887" ID="Freemind_Link_1230747839" MODIFIED="1139446274448" TEXT="Address your organisational needs">
+<node CREATED="1150238320250" ID="Freemind_Link_1960570678" MODIFIED="1150238326062" TEXT="Learn New Skill"/>
+<node CREATED="1150238326593" ID="Freemind_Link_1102553897" MODIFIED="1164074206331" TEXT="Enter new sector"/>
+<node CREATED="1150238379437" ID="Freemind_Link_202822827" MODIFIED="1150238384703" TEXT="Get a reference"/>
+</node>
+</node>
+</node>
+<node CREATED="1139395557837" ID="Freemind_Link_325567240" MODIFIED="1150237504640" POSITION="right" TEXT="Maintain Credibility">
+<arrowlink DESTINATION="Freemind_Link_580965216" ENDARROW="Default" ENDINCLINATION="79;0;" ID="Freemind_Arrow_Link_1259001882" STARTARROW="None" STARTINCLINATION="79;0;"/>
+<node CREATED="1139446409080" ID="Freemind_Link_1205225470" MODIFIED="1139651827161" TEXT="Truthful"/>
+<node CREATED="1139446387128" ID="Freemind_Link_1843236882" MODIFIED="1139738046485" TEXT="Quality"/>
+<node CREATED="1139452966013" ID="Freemind_Link_1805104613" MODIFIED="1139452969005" TEXT="Hard work"/>
+<node CREATED="1139395650110" ID="Freemind_Link_693097957" MODIFIED="1139395656148" TEXT="Keep up to date"/>
+<node CREATED="1141181254333" ID="Freemind_Link_1526672767" MODIFIED="1141182409724" TEXT="Style">
+<arrowlink DESTINATION="Freemind_Link_376511107" ENDARROW="Default" ENDINCLINATION="-30;15;" ID="Freemind_Arrow_Link_1503175607" STARTARROW="None" STARTINCLINATION="-199;80;"/>
+</node>
+</node>
+<node CREATED="1139394705907" HGAP="21" ID="Freemind_Link_1374203276" MODIFIED="1150259428296" POSITION="left" TEXT="Produce Consistant&#xa;Quality Deliverables" VSHIFT="84">
+<node CREATED="1139395079078" ID="Freemind_Link_1062578695" MODIFIED="1139446797330" TEXT="Turn  intagible into tangible">
+<arrowlink DESTINATION="_" ENDARROW="Default" ENDINCLINATION="483;0;" ID="Freemind_Arrow_Link_1506649527" STARTARROW="None" STARTINCLINATION="483;0;"/>
+</node>
+<node CREATED="1139395187584" ID="Freemind_Link_1068968651" MODIFIED="1139395193884" TEXT="Know what they are"/>
+<node CREATED="1139395112126" ID="Freemind_Link_183269084" MODIFIED="1139395127338" TEXT="Document everything"/>
+<node CREATED="1139395141618" ID="Freemind_Link_1999573138" MODIFIED="1141182389325" TEXT="Templates">
+<node CREATED="1139395149490" ID="Freemind_Link_629383647" MODIFIED="1139395154917" TEXT="Makes it look good"/>
+<node CREATED="1139395155488" ID="Freemind_Link_1293005047" MODIFIED="1139395173354" TEXT="Reduces effort">
+<node CREATED="1150259442953" ID="Freemind_Link_1551835230" MODIFIED="1150259453234" TEXT="Lowers your costs">
+<edge WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node CREATED="1139395196958" ID="Freemind_Link_1700197974" MODIFIED="1139395200803" TEXT="Project management">
+<node CREATED="1139395201434" ID="Freemind_Link_78381358" MODIFIED="1139403681428" TEXT="Know who&apos;s managing">
+<node CREATED="1139403682171" ID="Freemind_Link_148865091" MODIFIED="1139403683656" TEXT="You"/>
+<node CREATED="1139403685022" ID="Freemind_Link_170793526" MODIFIED="1139403686688" TEXT="Client"/>
+</node>
+<node CREATED="1139454001973" ID="Freemind_Link_1993336078" MODIFIED="1139454004674" TEXT="Planning">
+<node CREATED="1141181953668" ID="Freemind_Link_1531826078" MODIFIED="1146176491305" TEXT="Identify VIsion,&#xa;Objective and&#xa;Tasks"/>
+<node CREATED="1141181959637" ID="Freemind_Link_343427379" MODIFIED="1141182024370" TEXT="Assign Resources"/>
+<node CREATED="1141181964384" ID="Freemind_Link_522503077" MODIFIED="1141181968249" TEXT="Schedule"/>
+<node CREATED="1141181969621" ID="Freemind_Link_449804824" MODIFIED="1141181971925" TEXT="Tracking"/>
+<node CREATED="1141181976852" ID="Freemind_Link_1179577506" MODIFIED="1141182007055" TEXT="Correction"/>
+<node CREATED="1149806174937" ID="Freemind_Link_159062923" MODIFIED="1149806179968" TEXT="Risk Management"/>
+</node>
+<node CREATED="1139395209075" ID="Freemind_Link_801758766" MODIFIED="1139395212941" TEXT="Weekly report">
+<node CREATED="1139395213642" ID="Freemind_Link_882840747" MODIFIED="1139395275220" TEXT="Issues/actions/variance"/>
+<node CREATED="1139395221944" ID="Freemind_Link_100822177" MODIFIED="1139395231818" TEXT="Progress"/>
+<node CREATED="1139395234001" ID="Freemind_Link_556473641" MODIFIED="1139395239469" TEXT="Planned activity"/>
+<node CREATED="1139395241252" ID="Freemind_Link_189357050" MODIFIED="1139395249514" TEXT="Budget"/>
+<node CREATED="1139395259438" ID="Freemind_Link_1683147979" MODIFIED="1139395266037" TEXT="Other info"/>
+</node>
+<node CREATED="1139395290923" ID="Freemind_Link_1892756150" MODIFIED="1139395295089" TEXT="Change control">
+<node CREATED="1146176544504" ID="Freemind_Link_1199801452" MODIFIED="1146176596906" TEXT="Defined process to agree change in&#xa;cost, scope or schedule"/>
+</node>
+</node>
+</node>
+<node CREATED="1139394725465" ID="Freemind_Link_1190330451" MODIFIED="1141181765388" POSITION="left" TEXT="Work practice" VSHIFT="-5">
+<node CREATED="1139395429202" ID="Freemind_Link_376511107" MODIFIED="1141182409724" TEXT="Look the part">
+<node CREATED="1139395435411" ID="Freemind_Link_905716047" MODIFIED="1139446202871" TEXT="Reflect the habits of the customer"/>
+<node CREATED="1139395473546" ID="Freemind_Link_69433514" MODIFIED="1139395488277" TEXT="Business formal">
+<node CREATED="1139446204523" HGAP="31" ID="Freemind_Link_1787675" MODIFIED="1149805851843" TEXT="No one lost business&#xa;being overdressed" VSHIFT="-7"/>
+</node>
+</node>
+<node CREATED="1139395500064" ID="Freemind_Link_552209095" MODIFIED="1149806009156" TEXT="Confirm every&#xa;conversation in writing">
+<node CREATED="1139446692705" HGAP="33" ID="_" MODIFIED="1141182406009" TEXT="If it&apos;s not wiritten down&#xa;it does not exist" VSHIFT="-5">
+<node CREATED="1149805914812" ID="Freemind_Link_1125929454" MODIFIED="1149806032921" TEXT="Avoids&#xa;&quot;But I thought you said..&quot;"/>
+</node>
+<node CREATED="1149805876718" ID="Freemind_Link_622995841" MODIFIED="1149805894578" TEXT="Confirms understanding">
+<node CREATED="1149805914812" ID="Freemind_Link_1218623634" MODIFIED="1149805989921" TEXT="Avoids&#xa;&quot;But I thought you meant..&quot;"/>
+</node>
+<node CREATED="1149805895718" ID="Freemind_Link_411080089" MODIFIED="1149805905234" TEXT="CYOA later"/>
+</node>
+<node CREATED="1139403848032" ID="Freemind_Link_545717965" MODIFIED="1141182293367" TEXT="Get every deliverable&#xa;reviewed before issue">
+<arrowlink DESTINATION="Freemind_Link_1843236882" ENDARROW="Default" ENDINCLINATION="-95;60;" ID="Freemind_Arrow_Link_649032636" STARTARROW="None" STARTINCLINATION="-178;0;"/>
+<arrowlink DESTINATION="Freemind_Link_545717965" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Freemind_Arrow_Link_876340242" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<node CREATED="1141182104275" ID="Freemind_Link_658482171" MODIFIED="1141182134639" TEXT="Or issue as draft and&#xa;invite feedback"/>
+</node>
+<node CREATED="1139395602030" ID="Freemind_Link_1266864600" MODIFIED="1139395619265" TEXT="IT infrasture">
+<node CREATED="1139395620217" ID="Freemind_Link_282410263" MODIFIED="1139395621809" TEXT="Email"/>
+<node CREATED="1139395623061" ID="Freemind_Link_821420560" MODIFIED="1139395638102" TEXT="Disaster recovery"/>
+<node CREATED="1139403818577" ID="Freemind_Link_1709715046" MODIFIED="1141182389325" TEXT="Document production">
+<arrowlink DESTINATION="Freemind_Link_1999573138" ENDARROW="Default" ENDINCLINATION="405;-66;" ID="Freemind_Arrow_Link_1930352483" STARTARROW="None" STARTINCLINATION="482;-50;"/>
+<node CREATED="1139403825112" ID="Freemind_Link_1690169596" MODIFIED="1139403828495" TEXT="PDF"/>
+</node>
+<node CREATED="1139403880801" ID="Freemind_Link_152137373" MODIFIED="1139403888029" TEXT="Phone messaging"/>
+<node CREATED="1141181394054" ID="Freemind_Link_423900732" MODIFIED="1141181398340" TEXT="Remote Access"/>
+</node>
+<node CREATED="1139403922012" ID="Freemind_Link_642547845" MODIFIED="1139403927203" TEXT="Record Keeping">
+<node CREATED="1139403927966" ID="Freemind_Link_1872433936" MODIFIED="1139403933588" TEXT="Time Sheets">
+<node CREATED="1149806119234" ID="Freemind_Link_23445677" MODIFIED="1149806124703" TEXT="By task"/>
+</node>
+<node CREATED="1139403936489" ID="Freemind_Link_476086568" MODIFIED="1139403939200" TEXT="Expenses">
+<node CREATED="1146176244205" ID="Freemind_Link_1170338759" MODIFIED="1146176278858" TEXT="Seperate&#xa;credit card"/>
+</node>
+</node>
+<node CREATED="1139395329398" ID="Freemind_Link_71989696" MODIFIED="1141182564527" TEXT="Identify and report issues early">
+<arrowlink DESTINATION="Freemind_Link_580965216" ENDARROW="Default" ENDINCLINATION="611;244;" ID="Freemind_Arrow_Link_1850228432" STARTARROW="None" STARTINCLINATION="-200;194;"/>
+<arrowlink DESTINATION="Freemind_Link_325567240" ENDARROW="Default" ENDINCLINATION="176;-94;" ID="Freemind_Arrow_Link_781009479" STARTARROW="None" STARTINCLINATION="-48;-27;"/>
+<node CREATED="1139395340985" ID="Freemind_Link_1564532385" MODIFIED="1139446832992" TEXT="Good news does not get better"/>
+<node CREATED="1139395358180" ID="Freemind_Link_1319965611" MODIFIED="1139395372000" TEXT="The later it&apos;s left the larger the impact"/>
+<node CREATED="1149806142750" ID="Freemind_Link_584812028" MODIFIED="1149806152312" TEXT="Do it in writing"/>
+<node CREATED="1149806210984" ID="Freemind_Link_487823843" MODIFIED="1149806218875" TEXT="Reference the risk register"/>
+</node>
+<node CREATED="1146176169363" ID="Freemind_Link_1074290299" MODIFIED="1146176189518" TEXT="Avoid the politics">
+<node CREATED="1146176190922" ID="Freemind_Link_515394063" MODIFIED="1146176202928" TEXT="Need to listen and be aware">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1146176204475" ID="Freemind_Link_117841670" MODIFIED="1146176222098" TEXT="Avoid taking sides or&#xa;having a opnion"/>
+</node>
+</node>
+<node CREATED="1139394811699" ID="Freemind_Link_288149217" MODIFIED="1141182406009" POSITION="right" TEXT="Manage Risks">
+<arrowlink DESTINATION="_" ENDARROW="Default" ENDINCLINATION="-175;-17;" ID="Freemind_Arrow_Link_1864983042" STARTARROW="None" STARTINCLINATION="-114;52;"/>
+<node CREATED="1141348186330" ID="Freemind_Link_1298672335" MODIFIED="1141348370744" TEXT="How">
+<node CREATED="1141348371523" ID="Freemind_Link_116845507" MODIFIED="1141348389334" TEXT="List all specific risks"/>
+</node>
+<node CREATED="1139394817567" ID="Freemind_Link_131527782" MODIFIED="1139394823936" TEXT="Clients risks"/>
+<node CREATED="1139394824657" ID="Freemind_Link_1653441562" MODIFIED="1139394838938" TEXT="Your risks"/>
+</node>
+<node CREATED="1139395663869" ID="Freemind_Link_580965216" MODIFIED="1141182564527" POSITION="right" TEXT="Meet expecations">
+<node CREATED="1139394797178" ID="Freemind_Link_661526215" MODIFIED="1139395024360" TEXT="Retest assumptions"/>
+<node CREATED="1139446344424" ID="Freemind_Link_3892249" MODIFIED="1141214962131" TEXT="If you exceed expectations&#xa;was your plan bad?"/>
+</node>
+<node CREATED="1139651270150" ID="Freemind_Link_205961888" MODIFIED="1150237516328" POSITION="right" TEXT="Refine Skills">
+<node CREATED="1139651277211" ID="Freemind_Link_1198541548" MODIFIED="1139651286684" TEXT="50% Technical"/>
+<node CREATED="1139651287886" ID="Freemind_Link_1031122251" MODIFIED="1139651293855" TEXT="50% Delivery"/>
+</node>
+<node CREATED="1141181466468" ID="Freemind_Link_1135493615" MODIFIED="1141181478555" POSITION="right" TEXT="Use Bench time">
+<node CREATED="1141181887573" ID="Freemind_Link_516611291" MODIFIED="1141181895224" TEXT="Run your business"/>
+<node CREATED="1141181479476" ID="Freemind_Link_1592219" MODIFIED="1141181485044" TEXT="Self learning">
+<node CREATED="1146178221591" ID="Freemind_Link_379590817" MODIFIED="1146178237891" TEXT="Technical and Delivery"/>
+</node>
+<node CREATED="1141181488970" ID="Freemind_Link_1580707236" MODIFIED="1141181505294" TEXT="Sharpen your saw">
+<node CREATED="1146178200731" ID="Freemind_Link_1595789665" MODIFIED="1146178209710" TEXT="Improve work pratices"/>
+</node>
+</node>
+<node CREATED="1139446729039" ID="Freemind_Link_1830402670" MODIFIED="1141182285626" POSITION="right" TEXT="Legal">
+<arrowlink DESTINATION="_" ENDARROW="Default" ENDINCLINATION="-100;12;" ID="Freemind_Arrow_Link_852184861" STARTARROW="None" STARTINCLINATION="-37;7;"/>
+<node CREATED="1139446732214" ID="Freemind_Link_758516978" MODIFIED="1139446734698" TEXT="IP"/>
+<node CREATED="1139446736641" ID="Freemind_Link_1796919112" MODIFIED="1167339213184" TEXT="Liability and indemenity"/>
+<node CREATED="1150237566062" ID="Freemind_Link_236388142" MODIFIED="1150237580468" TEXT="Financial Practice"/>
+</node>
+<node CREATED="1141348413089" ID="Freemind_Link_730739988" MODIFIED="1150237637640" POSITION="right" TEXT="Have process&#xa;for everything">
+<node CREATED="1149805546921" ID="Freemind_Link_1859457766" MODIFIED="1149805559609" TEXT="Why">
+<node CREATED="1141349466763" ID="Freemind_Link_256224740" MODIFIED="1150238145390" TEXT="Stops you forgetting things"/>
+<node CREATED="1146176360468" ID="Freemind_Link_71228242" MODIFIED="1149805720140" TEXT="Improves the quality&#xa;and reduces the effort ($)"/>
+<node CREATED="1141349478656" ID="Freemind_Link_516399671" MODIFIED="1150238170812" TEXT="Make it quicker and easier&#xa;to do the tedious stuff"/>
+</node>
+<node CREATED="1149805574468" ID="Freemind_Link_1937489492" MODIFIED="1149805578968" TEXT="How">
+<node CREATED="1149805588062" ID="Freemind_Link_1823417774" MODIFIED="1150238186875" TEXT="Check-lists"/>
+<node CREATED="1149805595921" ID="Freemind_Link_1302336308" MODIFIED="1149805599859" TEXT="Templates"/>
+<node CREATED="1149805607421" ID="Freemind_Link_894186499" MODIFIED="1149805611125" TEXT="Tools">
+<node CREATED="1149805612203" ID="Freemind_Link_1686676366" MODIFIED="1149805614906" TEXT="RT?"/>
+</node>
+</node>
+<node CREATED="1146176330318" ID="Freemind_Link_1609645449" MODIFIED="1150237782156" TEXT="Ongoing Review&#xa;and process Improvement"/>
+</node>
+</node>
+</map>
