dfc748ea0f6c2c8de623119e13283104502d00fc
Alec Clews <alec.clews@voga.com.au>
2010-01-07 20:59:33 +1100

    Open Source Startup
    Some notes I made for an informal presentation for OSDClub Melbourne
    on my experiences with a startup

---
+++ OpenSourceStartUp/Open Source Start-up.mm
@@ -0,0 +1,148 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1160541558742" ID="Freemind_Link_379783223" MODIFIED="1160541570778" TEXT="Open Source Start-up">
+<node CREATED="1160543776717" FOLDED="true" ID="Freemind_Link_15132444" MODIFIED="1160543780454" POSITION="right" TEXT="Introduction">
+<node CREATED="1160543827688" ID="Freemind_Link_14901769" MODIFIED="1160543837499" TEXT="None of this is advice"/>
+<node CREATED="1160543782033" ID="Freemind_Link_469136741" MODIFIED="1160543824855" TEXT="All material based on my own&#xa;experience and predujice"/>
+</node>
+<node CREATED="1160541572141" FOLDED="true" ID="_" MODIFIED="1160541577790" POSITION="right" TEXT="Having an idea">
+<node CREATED="1160541578739" ID="Freemind_Link_1749981905" MODIFIED="1160541587840" TEXT="Scratching your own itch"/>
+<node CREATED="1160541588744" ID="Freemind_Link_759656873" MODIFIED="1160541610886" TEXT="Scraching somone elses itch"/>
+<node CREATED="1160541660600" FOLDED="true" ID="Freemind_Link_1601246365" MODIFIED="1160544162892" TEXT="Real Market">
+<icon BUILTIN="help"/>
+<node CREATED="1160541611729" FOLDED="true" ID="Freemind_Link_1814657222" MODIFIED="1160541617877" TEXT="Research">
+<node CREATED="1160541618906" ID="Freemind_Link_729014361" MODIFIED="1160544170083" TEXT="Other products"/>
+<node CREATED="1160541624398" ID="Freemind_Link_783185464" MODIFIED="1160541627322" TEXT="Blogs"/>
+<node CREATED="1160541630778" ID="Freemind_Link_1482497765" MODIFIED="1160541632518" TEXT="Web"/>
+<node CREATED="1160541633987" ID="Freemind_Link_711574928" MODIFIED="1160544178465" TEXT="People Network"/>
+</node>
+</node>
+</node>
+<node CREATED="1160541753022" FOLDED="true" ID="Freemind_Link_1635378167" MODIFIED="1160541755069" POSITION="right" TEXT="A team">
+<node CREATED="1160542772626" FOLDED="true" ID="Freemind_Link_293267783" MODIFIED="1160542780402" TEXT="Skill areas">
+<node CREATED="1160541755826" ID="Freemind_Link_1422684240" MODIFIED="1160541761159" TEXT="Selling/Markting"/>
+<node CREATED="1160541761875" FOLDED="true" ID="Freemind_Link_1622335236" MODIFIED="1160541767841" TEXT="Creating the product">
+<node CREATED="1160541768559" ID="Freemind_Link_1367406068" MODIFIED="1160541772374" TEXT="Technology"/>
+<node CREATED="1160541772795" ID="Freemind_Link_1331942435" MODIFIED="1160541774816" TEXT="Managment"/>
+</node>
+<node CREATED="1160541778216" ID="Freemind_Link_1986153487" MODIFIED="1160542172350" TEXT="Financial Control and&#xa;business management"/>
+</node>
+<node CREATED="1160542039662" FOLDED="true" ID="Freemind_Link_300895744" MODIFIED="1160542048324" TEXT="Beware of working with friends">
+<node CREATED="1160542049201" ID="Freemind_Link_435418683" MODIFIED="1160542156789" TEXT="They may not stay friends for long"/>
+</node>
+<node CREATED="1160542064546" FOLDED="true" ID="Freemind_Link_1797811942" MODIFIED="1160542074789" TEXT="Must be open and honest">
+<node CREATED="1160542075586" ID="Freemind_Link_1326629138" MODIFIED="1160542105349" TEXT="Some things happen fast and hot"/>
+</node>
+<node CREATED="1160542109712" FOLDED="true" ID="Freemind_Link_1317507496" MODIFIED="1160542123224" TEXT="Must trust/respect each other">
+<node CREATED="1160542123989" ID="Freemind_Link_650948624" MODIFIED="1160542150768" TEXT="Still probe assumptions&#xa;and ask questions"/>
+</node>
+</node>
+<node CREATED="1160541863993" FOLDED="true" ID="Freemind_Link_270833987" MODIFIED="1160541883457" POSITION="right" TEXT="Know why you are doing this">
+<node CREATED="1160541885735" ID="Freemind_Link_1291501579" MODIFIED="1160541890412" TEXT="Money"/>
+<node CREATED="1160541891175" ID="Freemind_Link_1936439601" MODIFIED="1160541897656" TEXT="Experience"/>
+<node CREATED="1160541904472" ID="Freemind_Link_256426704" MODIFIED="1160541908633" TEXT="Challange"/>
+<node CREATED="1160545708053" ID="Freemind_Link_1260074577" MODIFIED="1160545721083" TEXT="To be 1.0"/>
+</node>
+<node CREATED="1160542491692" FOLDED="true" ID="Freemind_Link_508211471" MODIFIED="1160542496041" POSITION="left" TEXT="Getting investment">
+<node CREATED="1160542497241" FOLDED="true" ID="Freemind_Link_693134947" MODIFIED="1160542511991" TEXT="Your own savings">
+<node CREATED="1160543980405" HGAP="47" ID="Freemind_Link_248296329" MODIFIED="1160545772542" TEXT="Protect your&#xa;home/family etc." VSHIFT="-12"/>
+</node>
+<node CREATED="1160542513046" FOLDED="true" ID="Freemind_Link_571853757" MODIFIED="1160542539277" TEXT="Angels">
+<node CREATED="1160542519110" ID="Freemind_Link_394676320" MODIFIED="1160542527902" TEXT="People in your network"/>
+<node CREATED="1160543506436" ID="Freemind_Link_1498047643" MODIFIED="1160543517574" TEXT="Angel &apos;clubs&apos;"/>
+<node CREATED="1160542606500" ID="Freemind_Link_820856661" MODIFIED="1160542636590" TEXT="Smaller investers&#xa;- seed money"/>
+</node>
+<node CREATED="1160542641822" FOLDED="true" ID="Freemind_Link_1708888721" MODIFIED="1160542651119" TEXT="Venture Capatlists">
+<node CREATED="1160542652166" ID="Freemind_Link_379136501" MODIFIED="1160542688072" TEXT="Large money &gt; US$2-3"/>
+<node CREATED="1160542689881" FOLDED="true" ID="Freemind_Link_513786712" MODIFIED="1160542694171" TEXT="Beware of terms">
+<node CREATED="1160542695813" ID="Freemind_Link_937603073" MODIFIED="1160542701178" TEXT="They can control your company"/>
+</node>
+<node CREATED="1160542707682" FOLDED="true" ID="Freemind_Link_1880827167" MODIFIED="1160542720273" TEXT="Hard to get in front off">
+<node CREATED="1160542722187" ID="Freemind_Link_600237042" MODIFIED="1160542737616" TEXT="People will help you, for&#xa;a share in the company"/>
+</node>
+</node>
+</node>
+<node CREATED="1160544887702" FOLDED="true" ID="Freemind_Link_1385249556" MODIFIED="1160556423197" POSITION="left" TEXT="Where is the centre&#xa;of OSS Universe?">
+<node CREATED="1160544900163" ID="Freemind_Link_1009860779" MODIFIED="1160544909650" TEXT="US has more legal expertise"/>
+<node CREATED="1160544924080" ID="Freemind_Link_996109985" MODIFIED="1160544942520" TEXT="US has more investers"/>
+<node CREATED="1160544949258" ID="Freemind_Link_31351664" MODIFIED="1160544955306" TEXT="US has more customers"/>
+<node CREATED="1160544965943" FOLDED="true" ID="Freemind_Link_1705486717" MODIFIED="1160544980723" TEXT="US has more people creating OSS">
+<node CREATED="1160545004741" ID="Freemind_Link_197018899" MODIFIED="1160545021051" TEXT="But there are lots&#xa;everywhere else as well"/>
+</node>
+</node>
+<node CREATED="1160542388006" FOLDED="true" ID="Freemind_Link_406866331" MODIFIED="1160542400024" POSITION="left" TEXT="Communication">
+<node CREATED="1160542400739" FOLDED="true" ID="Freemind_Link_741650260" MODIFIED="1160542402908" TEXT="Networking">
+<node CREATED="1160542403615" ID="Freemind_Link_1170221323" MODIFIED="1160542412968" TEXT="Elevator pitch"/>
+</node>
+<node CREATED="1160542415984" FOLDED="true" ID="Freemind_Link_1487882318" MODIFIED="1160542421924" TEXT="Presentations">
+<node CREATED="1160542422836" ID="Freemind_Link_1512890628" MODIFIED="1160542428595" TEXT="For customers"/>
+<node CREATED="1160542429036" ID="Freemind_Link_1337552303" MODIFIED="1160542435059" TEXT="Investers"/>
+</node>
+<node CREATED="1160542445509" ID="Freemind_Link_555115623" MODIFIED="1160542448434" TEXT="Website"/>
+<node CREATED="1160543003252" FOLDED="true" ID="Freemind_Link_1335112381" MODIFIED="1160543020198" TEXT="Getting the product&#xa;in front of people">
+<node CREATED="1160543021685" ID="Freemind_Link_717304900" MODIFIED="1160543036305" TEXT="c.o.l.a."/>
+<node CREATED="1160543037026" ID="Freemind_Link_437498720" MODIFIED="1160543042906" TEXT="NewsForge"/>
+<node CREATED="1160543056224" ID="Freemind_Link_1355492208" MODIFIED="1160543060061" TEXT="You tell me"/>
+<node CREATED="1160543075169" ID="Freemind_Link_1356522267" MODIFIED="1160543085048" TEXT="All about the downloads"/>
+</node>
+</node>
+<node CREATED="1160542178197" FOLDED="true" ID="Freemind_Link_1034290343" MODIFIED="1160542205779" POSITION="left" TEXT="OSS License policy">
+<node CREATED="1160542207462" FOLDED="true" ID="Freemind_Link_377442845" MODIFIED="1160542212424" TEXT="It&apos;s a tar pit">
+<node CREATED="1160542214579" ID="Freemind_Link_1707559922" MODIFIED="1160542229757" TEXT="No case law&#xa;or experience"/>
+</node>
+<node CREATED="1160542233672" ID="Freemind_Link_1496797212" MODIFIED="1160542359244" TEXT="Investers and suites&#xa;are scared of &apos;viral&apos;&#xa;licences i.e. GPL"/>
+<node CREATED="1160542280463" ID="Freemind_Link_1434043074" MODIFIED="1160542319418" TEXT="Need to be compatible with&#xa;any embedded OSS products "/>
+<node CREATED="1160542833114" ID="Freemind_Link_1494307236" MODIFIED="1160542850530" TEXT="Use OSI License, don&apos;t re-invent the wheel"/>
+</node>
+<node CREATED="1160542938549" FOLDED="true" ID="Freemind_Link_1396891380" MODIFIED="1160542941782" POSITION="left" TEXT="Schedules">
+<node CREATED="1160542942557" ID="Freemind_Link_1528314" MODIFIED="1160542960811" TEXT="Often times of frustration&#xa;when nothing happens"/>
+<node CREATED="1160542963405" FOLDED="true" ID="Freemind_Link_1732189508" MODIFIED="1160542968165" TEXT="Use downtime">
+<node CREATED="1160542969194" ID="Freemind_Link_1771586133" MODIFIED="1160542986466" TEXT="Marketing material"/>
+<node CREATED="1160542987039" ID="Freemind_Link_1964194826" MODIFIED="1160542995109" TEXT="Dev infrastructure"/>
+</node>
+<node CREATED="1160543133881" ID="Freemind_Link_941900645" MODIFIED="1160543146910" TEXT="Be flexible -- it takes longer than you think"/>
+</node>
+<node CREATED="1160543869235" FOLDED="true" ID="Freemind_Link_631757119" MODIFIED="1160543884811" POSITION="left" TEXT="Final Points">
+<node CREATED="1160543885587" ID="Freemind_Link_932447805" MODIFIED="1160543890267" TEXT="Be flexible"/>
+<node CREATED="1160543890800" ID="Freemind_Link_1059327508" MODIFIED="1160543898895" TEXT="Be ready for the stress"/>
+<node CREATED="1160543911334" ID="Freemind_Link_932636186" MODIFIED="1160543917513" TEXT="Know why you are doing this"/>
+<node CREATED="1160543918005" ID="Freemind_Link_1450449182" MODIFIED="1160544079453" TEXT="Accept that it will&#xa;probably fail"/>
+<node CREATED="1160543926366" FOLDED="true" ID="Freemind_Link_52220777" MODIFIED="1160543933693" TEXT="Believe it&apos;s worth doing">
+<node CREATED="1160544036725" ID="Freemind_Link_958247311" MODIFIED="1160544068034" TEXT="Make sure your important&#xa;people also think it&apos;s worth it"/>
+</node>
+</node>
+<node CREATED="1160541679165" FOLDED="true" ID="Freemind_Link_1109926389" MODIFIED="1160541682294" POSITION="right" TEXT="Financials">
+<node CREATED="1160541683062" FOLDED="true" ID="Freemind_Link_574045403" MODIFIED="1160541690940" TEXT="Business Plan">
+<node CREATED="1160541691746" FOLDED="true" ID="Freemind_Link_1017943771" MODIFIED="1160541696908" TEXT="Financials">
+<node CREATED="1160541709698" ID="Freemind_Link_1392327892" MODIFIED="1160541714175" TEXT="Market opportunity"/>
+<node CREATED="1160541715192" FOLDED="true" ID="Freemind_Link_1413959447" MODIFIED="1160541719543" TEXT="Costs">
+<node CREATED="1160542864732" FOLDED="true" ID="Freemind_Link_543299995" MODIFIED="1160542882015" TEXT="Professional services">
+<node CREATED="1160542883033" ID="Freemind_Link_1209758381" MODIFIED="1160542885493" TEXT="Lawyers"/>
+<node CREATED="1160542887361" ID="Freemind_Link_1993921669" MODIFIED="1160542900960" TEXT="PR/artwork/website/etc"/>
+<node CREATED="1160542912307" ID="Freemind_Link_52989319" MODIFIED="1160542919822" TEXT="Accounting"/>
+<node CREATED="1160542920355" ID="Freemind_Link_131397190" MODIFIED="1160542926928" TEXT="Bank Charges"/>
+</node>
+</node>
+<node CREATED="1160541947034" ID="Freemind_Link_1933526550" MODIFIED="1160541951453" TEXT="Investment plan"/>
+</node>
+<node CREATED="1160541697811" ID="Freemind_Link_1390985053" MODIFIED="1160541705340" TEXT="Assumptions"/>
+<node CREATED="1160541724564" FOLDED="true" ID="Freemind_Link_653565447" MODIFIED="1160541730981" TEXT="Your value">
+<node CREATED="1160541731789" FOLDED="true" ID="Freemind_Link_1898103085" MODIFIED="1160541749886" TEXT="Why will you succed?">
+<node CREATED="1160545099404" ID="Freemind_Link_1594065971" MODIFIED="1160545102201" TEXT="Your team"/>
+</node>
+</node>
+</node>
+<node CREATED="1160542475459" ID="Freemind_Link_947381373" MODIFIED="1160542488359" TEXT="Make sure the money&#xa;is managed"/>
+</node>
+<node CREATED="1160541838315" FOLDED="true" ID="Freemind_Link_843920806" MODIFIED="1160541844875" POSITION="right" TEXT="Revenue">
+<node CREATED="1160541845776" FOLDED="true" ID="Freemind_Link_1224739646" MODIFIED="1160541851760" TEXT="Licence sales">
+<node CREATED="1160541852744" ID="Freemind_Link_127446877" MODIFIED="1160541860531" TEXT="But it&apos;s Open Source"/>
+</node>
+<node CREATED="1160541929869" FOLDED="true" ID="Freemind_Link_1412471032" MODIFIED="1160541933082" TEXT="Consulting">
+<node CREATED="1160541935678" ID="Freemind_Link_546142806" MODIFIED="1160541940730" TEXT="Scale"/>
+</node>
+<node CREATED="1160541966872" ID="Freemind_Link_935929226" MODIFIED="1160541969100" TEXT="Support"/>
+<node CREATED="1160541975572" ID="Freemind_Link_856000712" MODIFIED="1160541979485" TEXT="Donation ware"/>
+</node>
+</node>
+</map>
