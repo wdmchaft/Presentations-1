ac21c69ac09433581d4a29a1862012709daeb541
Alec Clews <alec.clews@voga.com.au>
2010-09-06 11:58:39 +1000

    Started Linux Backup Presenation for SFD_MEL_10

---
+++ LinuxBackup/LinuxBackup.mm
@@ -0,0 +1,209 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node ID="ID_1276555896" CREATED="1283734957051" MODIFIED="1283735958204" COLOR="#000000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Linux
+    </p>
+    <p>
+      Backup
+    </p>
+  </body>
+</html></richcontent>
+<font SIZE="20"/>
+<hook NAME="MapStyle" max_node_width="600"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node TEXT="Types of Backup" POSITION="right" ID="ID_207403067" CREATED="1283734977818" MODIFIED="1283735958277" COLOR="#0033ff">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="Amount of data" ID="ID_920800497" CREATED="1283735435223" MODIFIED="1283735958300" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="Frequency of change" ID="ID_570262045" CREATED="1283735443683" MODIFIED="1283735958333" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="Affect of loss" ID="ID_1461198901" CREATED="1283735449832" MODIFIED="1283735958365" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node POSITION="right" ID="ID_288243172" CREATED="1283734996901" MODIFIED="1283736695639" COLOR="#0033ff">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Backup
+    </p>
+    <p>
+      Media
+    </p>
+  </body>
+</html>
+</richcontent>
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="Online" ID="ID_1296540646" CREATED="1283735002523" MODIFIED="1283735958500" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="e.g. DropBox" ID="ID_1183791499" CREATED="1283735031800" MODIFIED="1283735958506" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="Small media" ID="ID_425786248" CREATED="1283735005021" MODIFIED="1283735958526" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="USB Thumb Drives" ID="ID_608342217" CREATED="1283735018573" MODIFIED="1283735958533" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="Large Media" ID="ID_1403654528" CREATED="1283735084360" MODIFIED="1283735958554" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="FOrmat as NTFS" ID="ID_718687440" CREATED="1283735755335" MODIFIED="1283735958562" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+<node TEXT="Do not Encrypt" ID="ID_179298295" CREATED="1283735762940" MODIFIED="1283735958579" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="Another Machine" ID="ID_1540434071" CREATED="1283736022584" MODIFIED="1283736031991" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="Remote Storage" ID="ID_346107799" CREATED="1283736033247" MODIFIED="1283736041830" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="DVD/CD" ID="ID_696260515" CREATED="1283736448798" MODIFIED="1283736455126" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="Plan" POSITION="right" ID="ID_995351662" CREATED="1283734989907" MODIFIED="1283735958390" COLOR="#0033ff">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="How much" ID="ID_1774595682" CREATED="1283735044922" MODIFIED="1283735958430" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="When" ID="ID_281150543" CREATED="1283735047493" MODIFIED="1283735958443" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="Weekly?" ID="ID_429551967" CREATED="1283736527939" MODIFIED="1283736531719" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+<node TEXT="Daily?" ID="ID_1220181688" CREATED="1283736532575" MODIFIED="1283736536513" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+<node TEXT="Online?" ID="ID_1422064510" CREATED="1283736537320" MODIFIED="1283736540028" COLOR="#990000">
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="What" ID="ID_969866555" CREATED="1283735049651" MODIFIED="1283735958454" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node TEXT="Linux Operating System" ID="ID_906960713" CREATED="1283736329638" MODIFIED="1283736338592" COLOR="#990000">
+<font SIZE="14"/>
+<node TEXT="Settings?" ID="ID_107358683" CREATED="1283736559158" MODIFIED="1283736562087" COLOR="#111111">
+<font SIZE="12"/>
+</node>
+<node TEXT="Release DVD?" ID="ID_1011537671" CREATED="1283736553788" MODIFIED="1283736684600" COLOR="#111111">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Work in progress" ID="ID_1726888112" CREATED="1283736339094" MODIFIED="1283736355713" COLOR="#990000">
+<font SIZE="14"/>
+<node TEXT="Documents etc" ID="ID_1600361197" CREATED="1283736565746" MODIFIED="1283736571916" COLOR="#111111">
+<font SIZE="12"/>
+</node>
+<node TEXT="USB Drive?" ID="ID_1031219607" CREATED="1283736597916" MODIFIED="1283736604918" COLOR="#111111">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Keys, config settings" ID="ID_1482728009" CREATED="1283736356389" MODIFIED="1283736366705" COLOR="#990000">
+<font SIZE="14"/>
+<node TEXT="Multiple copies" ID="ID_311366508" CREATED="1283736637151" MODIFIED="1283736645429" COLOR="#111111">
+<font SIZE="12"/>
+</node>
+<node TEXT="CD/DVD + USB + Online" ID="ID_1847993903" CREATED="1283736645932" MODIFIED="1283736663537" COLOR="#111111">
+<font SIZE="12"/>
+</node>
+</node>
+<node TEXT="Music, books," ID="ID_859857941" CREATED="1283736367371" MODIFIED="1283736445064" COLOR="#990000">
+<font SIZE="14"/>
+<node TEXT="DVD" ID="ID_1914773390" CREATED="1283736665679" MODIFIED="1283736671699" COLOR="#111111">
+<font SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Assumptions" POSITION="left" ID="ID_1720081322" CREATED="1283735063574" MODIFIED="1283735958618" COLOR="#0033ff">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="Linux" ID="ID_521311819" CREATED="1283735071271" MODIFIED="1283735958657" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="Personal System" ID="ID_1060941593" CREATED="1283735399084" MODIFIED="1283735958678" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+<node ID="ID_784761604" CREATED="1283735407131" MODIFIED="1283735958722" COLOR="#990000">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Not enterprise<br/>backup plan
+    </p>
+  </body>
+</html></richcontent>
+<font SIZE="14"/>
+</node>
+</node>
+<node TEXT="Simple and quick" ID="ID_1420425011" CREATED="1283735631359" MODIFIED="1283735958760" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+<node TEXT="Tools" POSITION="left" ID="ID_1696211957" CREATED="1283735863727" MODIFIED="1283735958798" COLOR="#0033ff">
+<font SIZE="18"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<node TEXT="Drag and drop in file manager" ID="ID_76973929" CREATED="1283735906311" MODIFIED="1283735958888" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node ID="ID_715814196" CREATED="1283735867548" MODIFIED="1283736862256" COLOR="#00b439">
+<richcontent TYPE="NODE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font face="Century Schoolbook L" color="#f79609">cp -a &lt;src&gt; &lt;backup&gt;</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+<node TEXT="Back In Time" ID="ID_1270885705" CREATED="1283735780018" MODIFIED="1283735974383" LINK="http://backintime.le-web.org/" HGAP="17" VSHIFT="2" COLOR="#00b439">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
